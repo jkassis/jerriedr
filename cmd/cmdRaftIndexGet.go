b231b7a1d1ced60703f1cae0a07d566c7cbad8aa
Jeremy Kassis <jkassis@gmail.com>
2022-07-10 08:44:40 -0700

    cli shape right

--- cmd/cmdRaftIndexGet.go
+++ cmd/cmdRaftIndexGet.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/jkassis/jerrie/core"
@@ -9,44 +10,38 @@ import (
 	"github.com/spf13/viper"
 )
 
-const (
-	FLAG_INDEX = "index"
-)
-
 func init() {
 	// A general configuration object (feed with flags, conf files, etc.)
 	v := viper.New()
 
 	// CLI Command with flag parsing
 	c := &cobra.Command{
-		Use:   "raftIndexSet",
+		Use:   "raftIndexGet",
 		Short: "Set the index of the last processed raft proposal",
 		// Long:  ``,
 		Run: func(cmd *cobra.Command, args []string) {
-			CMDRaftIndexSet(v)
+			CMDRaftIndexGet(v)
 		},
 	}
 
 	CMDDBConfig(c, v)
 
-	c.PersistentFlags().IntP(FLAG_INDEX, "i", 0, "target index")
-	c.MarkPersistentFlagRequired(FLAG_INDEX)
-	v.BindPFlag(FLAG_INDEX, c.PersistentFlags().Lookup(FLAG_INDEX))
-
 	MAIN.AddCommand(c)
 }
 
-func CMDRaftIndexSet(v *viper.Viper) {
+func CMDRaftIndexGet(v *viper.Viper) {
 	dbBadger := CMDDBRun(v)
 
 	index := uint64(v.GetInt64(FLAG_INDEX))
-	writeErr := dbBadger.TxnW(func(dbTxn core.DBTxn) error {
+	writeErr := dbBadger.TxnR(func(dbTxn core.DBTxn) error {
 		proposalIdxK := kittie.DBRaftProposalIDXK
 		proposalIdxV := &core.DBInt64V{Value: index}
-		return dbTxn.ObjPut(proposalIdxK, proposalIdxV, 0)
+		return dbTxn.ObjGet(proposalIdxK, proposalIdxV)
 	})
 
 	if writeErr != nil {
 		log.Fatalf("%v", writeErr)
 	}
+
+	fmt.Printf("Raft proposal index in DB is %d", index)
 }
