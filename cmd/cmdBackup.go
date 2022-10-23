9be68d327d96b782cbdc110478b77a5366b3a888
Jeremy Kassis <jkassis@gmail.com>
2022-10-23 06:42:40 -0700

    add backup / restore

---
+++ cmd/cmdBackup.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"os"
+	"time"
+
+	"github.com/jkassis/jerrie/core"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	// A general configuration object (feed with flags, conf files, etc.)
+	v := viper.New()
+
+	// CLI Command with flag parsing
+	c := &cobra.Command{
+		Use:   "backup",
+		Short: "Backup the DB to STDOUT",
+		// Long:  ``,
+		Run: func(cmd *cobra.Command, args []string) {
+			CMDBackup(v)
+		},
+	}
+
+	CMDDBConfig(c, v)
+	MAIN.AddCommand(c)
+}
+
+func CMDBackup(v *viper.Viper) {
+	dbBadger := CMDDBRun(v)
+	stream := dbBadger.DB.NewStream()
+
+	start := time.Now()
+	core.Log.Warnf("DBBadgerSnapshot.Backup: starting")
+
+	// Run the stream (discard backup time)
+	_, err := stream.Backup(os.Stdout, 1)
+	if err != nil {
+		core.Log.Errorf("error doing badger db backup: %w", err)
+	}
+
+	duration := time.Since(start)
+	core.Log.Warnf("DBBadgerSnapshot.Backup: took %s", duration.String())
+}
---
+++ cmd/cmdRestore.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"os"
+	"time"
+
+	"github.com/jkassis/jerrie/core"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	// A general configuration object (feed with flags, conf files, etc.)
+	v := viper.New()
+
+	// CLI Command with flag parsing
+	c := &cobra.Command{
+		Use:   "restore",
+		Short: "Restore the DB from STDIN",
+		// Long:  ``,
+		Run: func(cmd *cobra.Command, args []string) {
+			CMDRestore(v)
+		},
+	}
+
+	CMDDBConfig(c, v)
+	MAIN.AddCommand(c)
+}
+
+func CMDRestore(v *viper.Viper) {
+	dbBadger := CMDDBRun(v)
+
+	start := time.Now()
+	core.Log.Warn("Restore: starting")
+	err := dbBadger.DB.Load(os.Stdin, 256)
+	if err != nil {
+		core.Log.Error(err)
+		return
+	}
+	duration := time.Since(start)
+	core.Log.Warnf("Restore: took %s", duration.String())
+}
