d056bf731e85a13788fd8344a11210e192e79b67
Jeremy Kassis <jkassis@gmail.com>
2023-01-01 12:27:15 +0800

    fix restore folder

--- cmd/prod.go
+++ cmd/prod.go
@@ -2,12 +2,12 @@ package main
 
 // conf for prod service
 var prodServiceSpecs []string = []string{
-	"statefulset|fg/dockie|10000|/v1/Backup|/v1/Restore|/var/data/single/<pod>-server-0",
-	"statefulset|fg/tickie|10000|/v1/Backup|/v1/Restore|/var/data/single/<pod>-server-0",
-	"statefulset|fg/ledgie|10000|/v1/Backup|/v1/Restore|/var/data/single/<pod>-server-0",
-	"statefulset|fg/dubbie|10000|/v1/Backup|/v1/Restore|/var/data/single/<pod>-server-0",
-	"statefulset|fg/keevie|10000|/v1/Backup|/v1/Restore|/var/data/single/<pod>-server-0",
-	"statefulset|fg/permie|10000|/v1/Backup|/v1/Restore|/var/data/single/<pod>-server-0",
+	"statefulset|fg/dockie|10000|/v1/Backup|/v1/Restore|/var/data/single/<pod>-server-0/restore",
+	"statefulset|fg/tickie|10000|/v1/Backup|/v1/Restore|/var/data/single/<pod>-server-0/restore",
+	"statefulset|fg/ledgie|10000|/v1/Backup|/v1/Restore|/var/data/single/<pod>-server-0/restore",
+	"statefulset|fg/dubbie|10000|/v1/Backup|/v1/Restore|/var/data/single/<pod>-server-0/restore",
+	"statefulset|fg/keevie|10000|/v1/Backup|/v1/Restore|/var/data/single/<pod>-server-0/restore",
+	"statefulset|fg/permie|10000|/v1/Backup|/v1/Restore|/var/data/single/<pod>-server-0/restore",
 }
 
 var prodSnapArchiveSpecs []string = []string{
